5b3030932fa36aaf514bd95b334d8fbf8a48ba92
masum184e <masum184e@gmail.com>
2025-04-20 10:58:33 +0600

    path error resolved

--- resources/views/dashboard/includes/layout.blade.php
+++ resources/views/dashboard/includes/layout.blade.php
@@ -134,7 +134,7 @@
             </a>
           </li>
           <li>
-            <a href="logout" class="flex items-center space-x-2 transition-all duration-200
+            <a href="/logout" class="flex items-center space-x-2 transition-all duration-200
               {{ Request::is('logout') ? 'text-blue-400 font-semibold pointer-events-none' : 'hover:text-blue-300' }}">
               <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round"
