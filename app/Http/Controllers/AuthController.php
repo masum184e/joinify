ba61ad0a3334508fc1a03ce828be7a5407e33ee4
masum184e <masum184e@gmail.com>
2025-04-21 14:58:53 +0600

    log removed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,6 +20,7 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required|min:6',
